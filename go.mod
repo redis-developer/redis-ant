34ddfa47980831f1f1f86a3eb9c7039a605e0693
Raman Tehlan <ramantehlan@gmail.com>
2021-05-15 08:44:12 +0530

    Add key_field int file to pick key for data

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-redis/redis/v8 v8.8.2
 	github.com/kelseyhightower/envconfig v1.4.0
+	go.mongodb.org/mongo-driver v1.5.2
 	gopkg.in/yaml.v2 v2.3.0
 )
--- internal/config/model.go
+++ internal/config/model.go
@@ -2,7 +2,20 @@ package config
 
 // AntConfig is the structure of the config used by RedisAnt program
 type AntConfig struct {
-	Database        string `yaml:"database"`
-	Collection      string `yaml:"collection"`
-	Fields 					[]string `yaml:"fields"`
+	Database   string   `yaml:"database"`
+	Collection string   `yaml:"collection"`
+	KeyField   string   `yaml:"key_field"`
+	Fields     []string `yaml:"fields"`
+
+	GlobalRedisURL  string `envconfig:"GLOBAL_REDIS_URL"`
+	GlobalRedisPass string `envconfig:"GLOBAL_REDIS_PASS"`
+	GlobalRedisPort string `envconfig:"GLOBAL_REDIS_PORT"`
+
+	ClientRedisURL  string `envconfig:"CLIENT_REDIS_URL"`
+	ClientRedisPass string `envconfig:"CLIENT_REDIS_PASS"`
+	ClientRedisPort string `envconfig:"CLIENT_REDIS_PORT"`
+
+	MongoURI  string `envconfig:"MONGO_URI"`
+	MongoUser string `envconfig:"MONGO_USER"`
+	MongoPass string `envconfig:"MONGO_PASS"`
 }
